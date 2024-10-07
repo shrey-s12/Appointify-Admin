04023082f35bcc03dbbcacdc6c0ec9190fc58761
Shrey Singhal <shreynbd12@gmail.com>
2024-10-07 23:43:11 +0530

    Create Dashboard page for Doctor panel

--- src/context/DoctorContext.jsx
+++ src/context/DoctorContext.jsx
@@ -10,6 +10,7 @@ const DoctorContextProvider = (props) => {
 
   const [dToken, setDToken] = useState(localStorage.getItem('dToken') ? localStorage.getItem('dToken') : false);
   const [appointments, setAppointments] = useState([]);
+  const [dashData, setDashData] = useState(false)
 
   const getAppointments = async () => {
     try {
@@ -56,13 +57,30 @@ const DoctorContextProvider = (props) => {
     }
   }
 
+  const getDashData = async () => {
+    try {
+      const { data } = await axios.get(`${backendUrl}/api/doctor/dashboard`, { headers: { dToken } });
+      if (data.success) {
+        setDashData(data.dashData);
+        console.log(data.dashData)
+      }
+      else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  }
+
 
   const value = {
     dToken, setDToken,
     backendUrl,
     appointments, setAppointments,
     getAppointments,
-    completeAppointment, cancelAppointment
+    completeAppointment, cancelAppointment,
+    dashData, setDashData,
+    getDashData
   };
 
   return (
